abd461bb509e13d7ae07a70defac71fda3fb32ee
Adam Bujalski <adam.bujalski14@gmail.com>
2025-11-23 09:36:57 +0100

    add print results

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ def main():
     book_num_words = get_num_words(book_text)
     book_num_chars = get_num_chars(book_text)
     sorted_list = sort_list(book_num_chars)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
     print(f"Found {book_num_words} total words")
-    print(sorted_list)
+    print("--------- Character Count -------")
+    for element in sorted_list:
+        if element["char"].isalpha() == True:
+            print(f'{element["char"]}: {element["num"]}')
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as book:
